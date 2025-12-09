8e9ca284d5bcd6cb43e82267b5a6dcae33d783ee
Luffy <luffydafloffi@gmail.com>
2025-12-09 18:59:27 +0200

    Remove some old code, add fuel system

--- Project/GAME2D.C
+++ Project/GAME2D.C
@@ -21,7 +21,9 @@ char entered_ascii_codes[ARRSIZE];
 int tail = -1;
 
 char ch_arr[ARRSIZE];
-int display_draft[25][80][2];
+int display_draft[25][80];
+int color_draft[25][80]; // Same as display draft but for coloring
+
 unsigned char far *b800h;
 int gun_position;           
 int no_of_arrows;
@@ -37,15 +39,12 @@ int point_in_cycle;
 int gcycle_length;
 int gno_of_pids;
 
-// POSITION target_pos[TARGET_NUMBER];
-// POSITION arrow_pos[ARROW_NUMBER];
-POSITION target_pos[ARRSIZE];
-POSITION arrow_pos[ARRSIZE];
-
 POSITION ship_pos;
+POSITION ship_vel;
+int fuel = 100;
 
 int flag = 0;
-int target_freq = 14;
+int target_freq = 10;
 
 
 /*CHANGE */
@@ -83,15 +82,11 @@ void interrupt new_int9(void)
     } //asm
 
     ascii = 0;
-    if (scan == 75) {
-        ascii = 'a';
-    } else if (scan == 72) {
+    if (scan == 0x48) { // Up arrow or W
         ascii = 'w';
-    } else if (scan == 77) {
-        ascii = 'd';
     }
-// if ((scan == 46)&& (ascii == 3)) // Ctrl-C?
-    if (scan == 1) { // Esc?
+
+    if (scan == 1) { // Esc
         my_halt(); // terminate program
     }
 
@@ -118,8 +113,8 @@ void displayer(void)
         for(col=0; col < 80; col++) {
             i = 2*(row*80 + col);
 
-            b800h[i] = display_draft[row][col][0];
-            b800h[i+1] = display_draft[row][col][1];
+            b800h[i] = display_draft[row][col];
+            b800h[i+1] = color_draft[row][col];
         } // for
     }
 } // displayer
@@ -148,42 +143,24 @@ void updateter_targets()
 {
     int i,j;
     static int initial_run = 1;
-    int no_of_targets;
 
-    if (initial_run == 1)
-    {
+    if (initial_run == 1) {
         initial_run = 0;
-        no_of_arrows = 0;
-
-        no_of_targets = 4;
-        target_pos[0].x = 3;
-        target_pos[0].y = 0; 
-
         ship_pos.x = 2;
         ship_pos.y = 0;
-
-        for(i=1; i < TARGET_NUMBER; i++)
-        {
-            target_pos[i].x = i*target_disp;
-            target_pos[i].y = 0; 
-
-        } // for
     } // if (initial_run == 1)
 
-
-//  while(1)
-//  {
-//   receive();
     if (flag == 0) {
-        ship_pos.x++;
-        ship_pos.y++;
+        // if (ship_pos.x < 80) {
+            ship_pos.x++;
+        // }
 
-        for(i=0; i < TARGET_NUMBER; i++) {
-            if (target_pos[i].x != -1) {
-                if (target_pos[i].y < 22) {
-                    target_pos[i].y++;    
-                }
-            } // if
+        if (ship_pos.y < 22) {
+            ship_pos.y += ship_vel.y;
+        }
+
+        if (ship_vel.y < 1) {
+            ship_vel.y++;
         }
     }
     flag++;
@@ -197,62 +174,38 @@ void updateter_targets()
 // Helper function to render something at some point in the screen
 // Instead of writing two lines
 void render_col(int row, int col, char c, int attr) {
-    display_draft[col][row][0] = c;
-    display_draft[col][row][1] = attr;
+    display_draft[col][row] = c;
+    color_draft[col][row] = attr;
 }
 void render(int row, int col, char c) {
-    display_draft[col][row][0] = c;
-}
-
-void update_ship() {
-    int x = ship_pos.x, y = ship_pos.y;
-    render(x, y, '_');
-
-    render(x-1, y+1, '(');
-    render(x, y+1, '_');
-    render(x+1, y+1, ')');
-    
-    render(x-1, y+2, '/');
-    render(x-2, y+2, '/');
-    render(x, y+2, '_');
-    render(x+1, y+2, '\\');
-    render(x+2, y+2, '\\');
-
-    // Will be the exhaust fire
-    // render_col(x-1, y+3, '\\', 0x0c);
-    // render_col(x+1, y+3, '/', 0x0c);
-    // render_col(x, y+4, 'v', 0x0c);
+    display_draft[col][row] = c;
 }
 
 void updater()
 {
     int i,j;
+    int show_exhaust = 0;
     static int initial_run = 1;
+    int x = ship_pos.x, y = ship_pos.y;
+    char ship[][6] = {
+        "  _",
+        " (_)",
+        "//_\\\\"
+    };
 
     if (initial_run == 1) {
         initial_run = 0;
-        no_of_arrows = 0;
-        no_of_targets = 4;
-        gun_position = 39;
-
         ship_pos.x = 2;
         ship_pos.y = 0;
-
-        for(i=0; i < ARROW_NUMBER; i++)
-            arrow_pos[i].x = arrow_pos[i].y = -1;
-    } // if (initial_run
+    }
 
     updateter_targets();
 
- // while(1)
- // {
-
-  // receive();
     ch = 0;
     for(i=0; i < 25; i++) {
         for(j=0; j < 80; j++) {
-            display_draft[i][j][0] = ' ';  // blank
-            display_draft[i][j][1] = 0x0f;  // blank
+            display_draft[i][j] = ' ';  // blank
+            color_draft[i][j] = 0x0f;  // blank
         }
     }
 
@@ -264,55 +217,30 @@ void updater()
             front = rear = -1;
         }
 
-        if ((ch == 'a') || (ch == 'A')) {
-            if (gun_position >= 2) {
-                gun_position--;
-                update_ship();
+        if ((ch == 'w' || ch == 'W') && fuel > 0) {
+            if (ship_vel.y > -1) {
+                ship_vel.y--;
             }
-        } else if ((ch == 'd') || (ch == 'D')) {
-            if (gun_position <= 78) {
-                gun_position++;
-                update_ship();
-            }
-        } else if ((ch == 'w') || (ch == 'W')) {
-            if (no_of_arrows < ARROW_NUMBER) {
-                arrow_pos[no_of_arrows].x = gun_position;
-                arrow_pos[no_of_arrows].y = 23;
-                no_of_arrows++;
-
-            } // if
+            show_exhaust = 1;
+            fuel--;
         }
     } // while(front != -1)
 
-    update_ship();
-
-    for(i=0; i < ARROW_NUMBER; i++) {
-        if (arrow_pos[i].x != -1) {
-            int j;
-            if (arrow_pos[i].y >= 0) {
-                arrow_pos[i].y--;
-                display_draft[arrow_pos[i].y][arrow_pos[i].x][0] = '^';
-                display_draft[arrow_pos[i].y+1][arrow_pos[i].x][0] = '|';
-            } // if
-
-            for(j=0; j < TARGET_NUMBER; j++) { /* CHANGE */
-                if ((arrow_pos[i].x == target_pos[j].x) && (arrow_pos[i].y <= target_pos[j].y)) {
-                    target_flags[j] = 0;
-                }
+    for (i = 0; i < 3; i++) {
+        char* s = ship[i];
+        for (j = 0; j < strlen(s); j++) {
+            char c = s[j];
+            if (c != ' ') {
+                display_draft[y + i][x - 2 + j] = c;
             }
-
-        } // if
+        }
     }
 
-    for(i=0; i < TARGET_NUMBER; i++) {
-        if (target_flags[i] == 1) { /*CHANGE */
-            if (target_pos[i].x != -1)
-            {
-                // CHANGE TO COMMENT if (target_pos[i].y < 22)
-                // CHANGE TO COMMENT      target_pos[i].y++;    
-                display_draft[target_pos[i].y][target_pos[i].x][0] = '*';
-            } // if
-        }
+    // Will be the exhaust fire
+    if (show_exhaust) {
+        render_col(x-1, y+3, '\\', 0x0c);
+        render_col(x+1, y+3, '/', 0x0c);
+        render_col(x, y+4, 'v', 0x0c);
     }
 } // updater 
 
@@ -322,8 +250,9 @@ void main() {
     int uppid, dispid, recvpid, targid;
     int i;
  
-    for(i=0; i< TARGET_NUMBER; i++)
+    for(i=0; i< TARGET_NUMBER; i++) {
         target_flags[i] = 1;
+    }
 
 
     asm {
@@ -340,17 +269,13 @@ void main() {
     old_int9 = getvect(9);
     setvect(9, new_int9);
 
-
-
-    while(1)
-    {
+    while(1) {
         displayer();
         receiver();
         updater();
    
         delay(1);
         // sleep(1);
-
     } // while
 
 
